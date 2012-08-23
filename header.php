2b026b0368b3757c7e17a80f4f590442c5645c4c
Gremlyn <gremlyn1@gmail.com>
2012-08-23 14:07:40 -0700

    Basic style layout for header.php with static top menu and secondary nav menu on page. Added style.css to store general layout requirements and overrides to Twitter Bootstrap where necessary.

--- header.php
+++ header.php
@@ -1,8 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-     <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
-     <link href="css/bootstrap.min.css" rel="stylesheet">
-     <script src="js/bootstrap.min.js"></script>
-     <?php wp_head(); ?>
+    <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+    <?php wp_head(); ?>
 </head>
+
+<body <?php body_class(); ?>>
+    <div class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+            <?php $defaults = array(
+                      'theme_location'  => '',
+                      'menu'            => '',
+                      'container'       => 'div',
+                      'container_class' => 'container-fluid',
+                      'container_id'    => '',
+                      'menu_class'      => 'nav nav-collapse pull-right',
+                      'menu_id'         => 'header-nav',
+                      'echo'            => true,
+                      'fallback_cb'     => 'wp_page_menu',
+                      'before'          => '',
+                      'after'           => '',
+                      'link_before'     => '',
+                      'link_after'      => '',
+                      'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                      'depth'           => 0,
+                      'walker'          => ''
+            ); ?>
+            <?php wp_nav_menu( $defaults ); ?>
+        </div>
+    </div>
+
+    <div class="navbar">
+        <div class="navbar-inner">
+            <?php $defaults = array(
+                      'theme_location'  => '',
+                      'menu'            => '',
+                      'container'       => 'div',
+                      'container_class' => 'container-fluid',
+                      'container_id'    => '',
+                      'menu_class'      => 'nav',
+                      'menu_id'         => 'header-nav',
+                      'echo'            => true,
+                      'fallback_cb'     => 'wp_page_menu',
+                      'before'          => '',
+                      'after'           => '',
+                      'link_before'     => '',
+                      'link_after'      => '',
+                      'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                      'depth'           => 0,
+                      'walker'          => ''
+            ); ?>
+            <?php wp_nav_menu( $defaults ); ?>
+        </div>
+    </div>
