f3f09a39c83e2807241d482bb95f117445ce9460
Gremlyn <gremlyn1@gmail.com>
2012-08-23 12:52:24 -0700

    Created basic theme structure files for WP and added Twitter Bootstrap 2.1.0 files.

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+     <link href="css/bootstrap.min.css" rel="stylesheet">
+     <script src="js/bootstrap.min.js"></script>
+     <?php wp_head(); ?>
+</head>
