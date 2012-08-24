bda02e3cb8f264786b7adb145feb457401e169f9
Gremlyn <gremlyn1@gmail.com>
2012-08-23 22:23:34 -0700

    Missing functions.php changes in the previous commit.

--- functions.php
+++ functions.php
@@ -1,52 +1,77 @@
 <?php
 
-register_nav_menu( 'header', 'Header Menu');
-register_nav_menu( 'primary', 'Primary Menu');
+require_once('settings.php');
+
+register_nav_menu('header', 'Header Menu');
+register_nav_menu('primary', 'Primary Menu');
 
 show_admin_bar( false );
 
-add_action( 'admin_init', 'theme_options_init' );
-add_action( 'admin_menu', 'theme_options_add_page' );
+add_action('admin_menu', 'twitstrap_settings_menu');
+add_action('admin_init', 'twitstrap_register_settings');
 
-function theme_options_init(){
-    register_setting( 'twitstrap_options', 'twitstrap_theme_options');
-}
-
-function theme_options_add_page() {
-    add_theme_page( __( 'Twitstrap Options', 'twitstrap' ),
-                    __( 'Twitstrap Options', 'twitstrap' ),
+function twitstrap_settings_menu() {
+    add_theme_page( __('Twitstrap Options', 'twitstrap'),
+                    __('Twitstrap Options', 'twitstrap'),
                     'edit_theme_options',
-                    'theme_options',
-                    'theme_options_do_page'
-                    );
+                    'twitstrap_settings',
+                    'twitstrap_render_settings');
 }
 
-function theme_options_do_page() {
-    global $select_options;
-    if ( ! isset( $_REQUEST['settings-updated'] ) ) {
-        $_REQUEST['settings-updated'] = false;
+function twitstrap_get_settings() {
+    $output = array();
+
+    $output['twitstrap_option_name']     = 'twitstrap_options'; // the option name as used in the get_option() call.
+    $output['twitstrap_page_title']      = __('Twitstrap Options', 'twitstrap'); // the settings page title
+    $output['twitstrap_page_sections']   = twitstrap_options_page_sections(); // the setting section
+    $output['twitstrap_page_fields']     = ''; // the setting fields
+    $output['twitstrap_contextual_help'] = ''; // the contextual help
+
+    return $output;
+}
+
+function twitstrap_render_settings() {
+    $settings_output = twitstrap_get_settings();
+    ?>
+    <div class="wrap">
+        <div class="icon32" id="icon-options-general"></div>
+        <h2><?php echo $settings_output['twitstrap_page_title']; ?></h2>
+
+        <form action="options.php" method="post">
+            <?php
+            settings_fields($settings_output['twitstrap_option_name']);
+            do_settings_sections(__FILE__);
+            ?>
+            <p class="submit">
+                <input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Changes','twitstrap'); ?>" />
+            </p>
+        </form>
+    </div><!-- wrap -->
+<?php
+}
+
+function twitstrap_register_settings() {
+    $settings_output = twitstrap_get_settings();
+    $twitstrap_option_name = $settings_output['twitstrap_option_name'];
+
+    register_setting($twitstrap_option_name,
+                     $twitstrap_option_name,
+                     'twitstrap_validate_options');
+
+    if( ! empty($settings_output['twitstrap_page_sections']) ) {
+        foreach ( $settings_output['twitstrap_page_sections'] as $id => $title ) {
+            add_settings_section( $id, $title, 'twitstrap_section_fn', __FILE__);
+        }
     }
 }
 
-add_action( 'init', 'create_post_type' );
+function twitstrap_validate_options($input) {
+    $valid_input = array();
+    return $valid_input;
+}
 
-function create_post_type() {
-    $carousel_args = array('labels' => array(
-                                             'name' => __( 'Carousels' ),
-                                             'singular_name' => __( 'Carousel' )
-                                             ),
-                           'description' => 'Posts to be displayed on rotation on the main page.',
-                           'public' => true,
-                           'show_in_nav_menus' => false,
-                           'capability_type' => 'page',
-                           'supports' => array(
-                                               'title',
-                                               'thumbnail',
-                                               'excerpt',
-                                               'page-attributes'
-                                               ),
-                           );
-    register_post_type( 'carousel', $carousel_args);
+function  twitstrap_section_fn($desc) {
+    echo "<p>" . __('Settings for this section','twitstrap') . "</p>";
 }
 
 ?>
