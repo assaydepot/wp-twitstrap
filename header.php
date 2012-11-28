83ff78cf32bb941cc31f12a159011f32e96f395d
Gremlyn <gremlyn1@gmail.com>
2012-11-28 15:06:03 -0800

    header.php was missing from previous commit.

--- header.php
+++ header.php
@@ -1,25 +1,28 @@
+<?php $twitstrap_options = twitstrap_get_global_options(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
-    <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?php bloginfo('url'); ?>/?custom_css=true" rel="stylesheet" type="text/css" />
     <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-    <?php show_admin_bar( true ); ?>
+  <?php
+  if ($twitstrap_options['twitstrap_header_menu'] == 1) {
+  ?>
     <div class="navbar navbar-fixed-top">
         <div class="navbar-inner">
             <?php $args = array(
-                      'theme_location'  => '',
-                      'menu'            => 'header',
+                      'theme_location'  => 'header_nav',
+                      'menu'            => 'header_nav',
                       'container'       => false,
                       'container_class' => '',
                       'container_id'    => '',
                       'menu_class'      => 'nav nav-collapse pull-right',
-                      'menu_id'         => 'header-nav',
+                      'menu_id'         => 'header_nav',
                       'echo'            => true,
                       'fallback_cb'     => 'wp_page_menu',
                       'before'          => '',
@@ -33,26 +36,31 @@
             <?php wp_nav_menu( $args ); ?>
         </div>
     </div>
+  <?php } ?>
 
     <div class="container">
         <div class="row-fluid">
             <div class="page-header span12">
-                <a href="<?php echo home_url(); ?>" title="<?php echo get_bloginfo('name').' - '.get_bloginfo('description'); ?>" id="logo-link">
-                   <h1><?php echo get_bloginfo('name').' <small>'.get_bloginfo('description'); ?></small></h1>
-                </a>
+                <h1>
+                    <a href="<?php echo home_url(); ?>" title="<?php echo get_bloginfo('name').' - '.get_bloginfo('description'); ?>" id="logo-link">
+                        <?php echo get_bloginfo('name') ?>
+                    </a><br />
+                <small><?php echo get_bloginfo('description'); ?></small></h1>
             </div>
         </div>
-
+      <?php
+      if ($twitstrap_options['twitstrap_page_menu'] == 1) {
+      ?>
         <div class="navbar">
             <div class="navbar-inner">
                 <?php $args = array(
-                          'theme_location'  => '',
-                          'menu'            => 'primary',
+                          'theme_location'  => 'inpage_nav',
+                          'menu'            => 'inpage_nav',
                           'container'       => false,
                           'container_class' => '',
                           'container_id'    => '',
                           'menu_class'      => 'nav',
-                          'menu_id'         => 'header-nav',
+                          'menu_id'         => 'inpage_nav',
                           'echo'            => true,
                           'fallback_cb'     => 'wp_page_menu',
                           'before'          => '',
@@ -66,4 +74,5 @@
                 <?php wp_nav_menu( $args ); ?>
             </div>
         </div>
+      <?php } ?>
     </div>
