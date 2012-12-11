046adb24fc217c0beaa97ff478b6e7cb3768c967
Gremlyn <gremlyn1@gmail.com>
2012-12-11 13:28:30 -0800

    Added function to catch and style images with captions in the body of a post.

--- functions.php
+++ functions.php
@@ -124,6 +124,51 @@ function twitstrap_comment($comment, $args, $depth) {
     endswitch;
 }
 
+/**
+ * Custom formatted captions
+ */
+add_filter( 'img_caption_shortcode', 'cleaner_caption', 10, 3 );
+
+function cleaner_caption( $output, $attr, $content ) {
+
+    // Not worried abut captions in feeds
+    if (is_feed())
+        return $output;
+
+    // Set up the default arguments
+    $defaults = array(
+                      'id' => '',
+                      'align' => 'alignnone',
+                      'width' => '',
+                      'caption' => ''
+                      );
+
+    // Merge the defaults with user input
+    $attr = shortcode_atts( $defaults, $attr );
+
+    // If the width is less than 1 or there is no caption, return the content between [caption] tags
+    if (1 > $attr['width'] || empty($attr['caption']))
+        return $content;
+
+    // Set up the attributes for the caption <div>
+    $attributes = (!empty( $attr['id']) ? ' id="' . esc_attr($attr['id']) . '"' : '' );
+    $attributes .= ' class="caption ' . esc_attr($attr['align']) . '"';
+
+    /* Open the caption <div>. */
+    $output = '<div class="thumbnail"><div' . $attributes .'>';
+
+    /* Allow shortcodes for the content the caption was created for. */
+    $output .= do_shortcode( $content );
+
+    /* Append the caption text. */
+    $output .= '<small>' . $attr['caption'] . '</small>';
+
+    /* Close the caption </div>. */
+    $output .= '</div></div>';
+
+    /* Return the formatted, clean caption. */
+    return $output;
+}
 
 /**
  * Option Page Stuff
